a488bc4a46001116124cba8eebcaecc1aba54dbf
EC2 Default User <ec2-user@ip-172-31-32-180.eu-north-1.compute.internal>
2026-02-02 16:36:47 +0000

    EDITED THE APP

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const http = require("http");
 
 const server = http.createServer((req, res) => {
   res.writeHead(200, { "Content-Type": "text/plain" });
-  res.end("i love you AFOLABI TEMI");
+  res.end(" I WILL BE GREAT IN LIFE");
 });
 
 server.listen(3000, "0.0.0.0", () => {
