eaee009aa41680e3bc7cc7806951a91c84e436cd
EC2 Default User <ec2-user@ip-172-31-34-73.eu-north-1.compute.internal>
2026-02-07 18:09:24 +0000

    Edited the app

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const http = require("http");
 
 const server = http.createServer((req, res) => {
   res.writeHead(200, { "Content-Type": "text/plain" });
-  res.end(" NEVER GIVE UP, IT JUST NEED A LITTLE PUSH");
+  res.end(" MY NAME IS GBOLAHAN");
 });
 
 server.listen(3000, "0.0.0.0", () => {
