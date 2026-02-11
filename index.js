7c761bf8f6de399190389240f13a6874ccf52013
EC2 Default User <ec2-user@ip-172-31-34-73.eu-north-1.compute.internal>
2026-02-11 10:09:22 +0000

    Initial app

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const http = require("http");
 
 const server = http.createServer((req, res) => {
   res.writeHead(200, { "Content-Type": "text/plain" });
-  res.end(" tested and approved");
+  res.end("tested and approved");
 });
 
 server.listen(3000, "0.0.0.0", () => {
